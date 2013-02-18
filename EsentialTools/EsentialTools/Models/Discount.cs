3c453f10fddee5ebd742ce750bfd5cdf00466509
tedydag@hotmail.com <tedydag@hotmail.com>
2013-02-18 12:47:31 -0600

    adding unit test

--- EsentialTools/EsentialTools/Models/Discount.cs
+++ EsentialTools/EsentialTools/Models/Discount.cs
@@ -34,4 +34,27 @@ namespace EsentialTools.Models
             return (totalParam - (discount / 100m * totalParam));
         }
     }
+
+    public class MinimumDiscountHelper : IDiscountHelper
+    {
+        public decimal ApplyDiscount(decimal totalParam)
+        {
+            if (totalParam < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else if (totalParam > 100)
+            {
+                return totalParam * 0.9M;
+            }
+            else if (totalParam >= 10 && totalParam <= 100)
+            {
+                return totalParam - 5;
+            }
+            else
+            {
+                return totalParam;
+            }
+        }
+    }
 }
