f81377c7dfb904268285efa3bbbcf46592fb1201
tedydag@hotmail.com <tedydag@hotmail.com>
2013-03-09 15:47:52 -0600

    chapter 7 sports store with entity framework used

---
+++ SportsStore/SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System.Linq;
+namespace SportsStore.Domain.Concrete
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private EFDbContext context = new EFDbContext();
+        public IQueryable<Product> Products
+        {
+            get { return context.Products; }
+        }
+    }
+}
--- SportsStore/SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -7,6 +7,7 @@ using System.Web.Routing;
 using Moq;
 using Ninject;
 using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
 using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
@@ -28,15 +29,6 @@ namespace SportsStore.WebUI.Infrastructure
         }
         private void AddBindings()
         {
-            Mock<IProductRepository> mock = new Mock<IProductRepository>();
-            mock.Setup(m => m.Products).Returns(new List<Product> {
-                new Product { Name = "Football", Price = 25 },
-                new Product { Name = "Surf board", Price = 179 },
-                new Product { Name = "Running shoes", Price = 95 }
-            }.AsQueryable());
-            ninjectKernel.Bind<IProductRepository>().ToConstant(mock.Object);
-
-
-        }
+            ninjectKernel.Bind<IProductRepository>().To<EFProductRepository>();        }
     }
 }
