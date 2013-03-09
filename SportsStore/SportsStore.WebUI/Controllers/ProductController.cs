ed0ca37641c40e77324de11b7fc5cb6dd7f143a7
tedydag@hotmail.com <tedydag@hotmail.com>
2013-03-09 17:11:04 -0600

    pagination

--- SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -9,15 +9,20 @@ namespace SportsStore.WebUI.Controllers
 {
     public class ProductController : Controller
     {
+        public int PageSize = 4;
+
         private IProductRepository repository;
         public ProductController(IProductRepository productRepository)
         {
             this.repository = productRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Products);
+            return View(repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize));
         }
     }
 }
